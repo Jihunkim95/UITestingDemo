909ce06c73fffadb51050f6dc72bd200fadcf07b
Jihunkim95 <darak551@naver.com>
2023-12-18 10:06:12 +0900

    add: UITestDemo 생성

---
+++ UITestingDemo/UITestingDemo/UITestingDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  UITestingDemoApp.swift
+//  UITestingDemo
+//
+//  Created by 김지훈 on 2023/12/18.
+//
+
+import SwiftUI
+
+@main
+struct UITestingDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
