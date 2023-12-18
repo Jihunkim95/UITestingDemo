693dab99358dd74b1393510383e8675ba0fff823
Jihunkim95 <darak551@naver.com>
2023-12-18 11:08:51 +0900

    refactor: 자주사용하는 XCUIApplication 전역으로

--- UITestingDemo/UITestingDemoUITests/UITestingDemoUITests.swift
+++ UITestingDemo/UITestingDemoUITests/UITestingDemoUITests.swift
@@ -9,23 +9,18 @@ import XCTest
 
 final class UITestingDemoUITests: XCTestCase {
 
+    // 전역 객체
+    let app = XCUIApplication()
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
 
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
     func testWelcome() throws {
-        let app = XCUIApplication()
         app.launch()
-
 //        let welcome = app.staticTexts["Welcome!"]
         // 현재 앱에 존재하는 유일한 텍스트 뷰
         let welcome = app.staticTexts.element
@@ -38,7 +33,6 @@ final class UITestingDemoUITests: XCTestCase {
 
     //로그인 버튼 Test
     func testLoginButton() throws {
-        let app = XCUIApplication()
         app.launch()
         
 //        let login = app.buttons["Login"]
